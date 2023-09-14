bf621f358380c634aca411765b30987b77f92ce0
Jere <jere.maennena@gmail.com>
2023-09-14 18:10:43 +0300

    add argument

---
+++ argument.cs
@@ -0,0 +1,13 @@
+// A program that prints the first given command-line argument. It is assumed that the argument is always provided and consists of letters and numbers.
+
+using System;
+
+class Argument
+{
+	
+	static void Main(string[] args)
+	{
+	
+	Console.WriteLine("Komentoriviargumentti oli: " + args[0]);
+	}
+}
