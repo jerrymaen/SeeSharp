762978eafd6959f824272812aee51fc8a01bd0e7
Jere <jere.maennena@gmail.com>
2023-09-19 17:51:13 +0300

    small fixes

---
+++ operators_2/operators_2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Introduce and initialize the variables x, y, z and b so that the program prints "Onnistui!".
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+
+        int x = 1;
+        int y = 2;
+        int z = 3;
+        int b = 4;
+        string s = "A";
+
+
+
+        if (((x >= y) && (y > z)) || ((x > y) && (s == "A")))
+        {
+            if (s == "C")
+            {
+                Console.WriteLine("Onnistui!");
+            }
+            else
+            {
+                Console.WriteLine("Melkein!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ei iha, yritä vielä!");
+        }
+    }
+}
