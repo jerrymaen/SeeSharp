588fc3124330e3d53de993749b8f0a956b304b67
Jere <jere.maennena@gmail.com>
2023-09-19 18:31:20 +0300

    small fixes

---
+++ operators/operators/Program.cs
@@ -0,0 +1,25 @@
+﻿// Introduce and initialize the variables a, b and c so that the program prints "Onnistui!".
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 1;
+        int b = 2;
+        int c = 0;
+
+        if ((a != b) && (a < b))
+        {
+            if (a > c)
+            {
+                Console.WriteLine("Onnistui!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Yritä uudestaan!");
+        }
+    }
+}
---
+++ variables/variables/Program.cs
@@ -0,0 +1,34 @@
+﻿//Declare the variables below and give them appropriate values:
+//var1 tyyppiä short.
+//var2 tyyppiä byte.
+//var3 tyyppiä float.   
+//var4 tyyppiä char.
+//var5 tyyppiä boolean.
+//var6 tyyppiä ulong.
+//var7 tyyppiä decimal.
+
+using System;
+
+class Tietotyypit
+{
+    static void Main(string[] args)
+    {
+
+        short var1 = 10;
+        byte var2 = 200;
+        float var3 = 3.14f;
+        char var4 = 'A';
+        bool var5 = true;
+        ulong var6 = 1000000000UL;
+        decimal var7 = 123.456m;
+
+        Console.WriteLine("Var1 on tyyppiä " + var1.GetType());
+        Console.WriteLine("Var2 on tyyppiä " + var2.GetType());
+        Console.WriteLine("Var3 on tyyppiä " + var3.GetType());
+        Console.WriteLine("Var4 on tyyppiä " + var4.GetType());
+        Console.WriteLine("Var5 on tyyppiä " + var5.GetType());
+        Console.WriteLine("Var6 on tyyppiä " + var6.GetType());
+        Console.WriteLine("Var7 on tyyppiä " + var7.GetType());
+
+    }
+}
