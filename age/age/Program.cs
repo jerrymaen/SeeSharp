82f8c5bbfeab1c61b6305ad2a7222ed75b7285af
Jere <jere.maennena@gmail.com>
2023-09-26 15:24:25 +0300

    small fixes

---
+++ age/age/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Syötä ikäsi, ole hyvä: ");
+        int ikä = Convert.ToInt32(Console.ReadLine());
+
+        if (ikä >= 18)
+        {
+            Console.WriteLine("Voit äänestää!");
+        }
+        Console.ReadLine();
+    }
+}
