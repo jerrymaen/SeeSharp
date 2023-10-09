9f70db24c034ac2dfbcde760a69146d0af3adc35
Jere <jere.maennena@gmail.com>
2023-10-09 19:31:17 +0300

    small fixes

--- largest_number/largest_number/Program.cs
+++ largest_number/largest_number/Program.cs
@@ -7,10 +7,27 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Syötä ensimmäinen numero:");
-        Console.ReadLine();
+        //Pyydetään käyttäjää syöttämään kaksi lukua
+        Console.Write("Syötä ensimmäinen numero: \n");
+        int numero1 = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Syötä toinen numero:");
-        Console.ReadLine();
+        Console.WriteLine("Syötä toinen numero: ");
+        int numero2 = int.Parse(Console.ReadLine());
+
+        //Jos ensimmäinen numero on isompi kuin toinen, tulostuu "Numero 1 on suurempi kuin numero 1."
+        if (numero1 > numero2)
+        {
+            Console.WriteLine("Numero 1 on suurempi kuin numero 2.");
+        }
+        //Jos toinen numero on isompi kuin ensimmäinen, tulostuu "Numero 2 on suurempi kuin numero 1."
+        else if (numero1 < numero2)
+        {
+            Console.WriteLine("Numero 2 on suurempi kuin numero 1.");
+        }
+        //Jos numerot ovat yhtä suuret, tulostuu "Numerot ovat yhtä suuret."
+        else
+        {
+            Console.WriteLine("Numerot ovat yhtä suuret.");
+        }
     }
 }
