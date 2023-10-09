b90381566fb100d10ea0690f74d62ac6638a8239
Jere <jere.maennena@gmail.com>
2023-10-09 16:13:14 +0300

    beginning

---
+++ largest_number/largest_number/Program.cs
@@ -0,0 +1,16 @@
+﻿//Program that asks the user to enter two integers and tells which of the numbers was larger, or if they were equal.
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Syötä ensimmäinen numero:");
+        Console.ReadLine();
+
+        Console.WriteLine("Syötä toinen numero:");
+        Console.ReadLine();
+    }
+}
