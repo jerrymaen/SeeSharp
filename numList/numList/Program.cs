810b5340ce2de19b1a038dde8584f037badbe57d
Jere Mäennenä <jere.maennena@gmail.com>
2024-03-05 00:45:57 +0200

    initial commit

---
+++ numList/numList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numerot = new List<int>(); // List-esittely
+
+        int syote;
+        while (true)
+        {
+            Console.Write("Syötä numero: ");
+            syote = int.Parse(Console.ReadLine());
+            if (syote < 0)
+                break;
+            // Elementtien lisäys listalle
+            numerot.Add(syote);
+        }
+
+        Console.Write("Syöttämäsi numerot: ");
+        foreach (int numero in numerot)
+        {
+            Console.Write(numero + " ");
+        }
+        Console.WriteLine();
+    }
+}
