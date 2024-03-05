126363ffb61e6fe1522b4d1a4eb318e1eda834a7
Jere Mäennenä <jere.maennena@gmail.com>
2024-03-05 20:11:52 +0200

    initial commit

---
+++ fileRead/fileRead/Program.cs
@@ -0,0 +1,26 @@
+﻿// Write a C# program that takes the name of a file as a command-line argument, and prints the contents of that file to the screen.
+
+using System;
+using System.IO;
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Ei tiedostonimeä!");
+            return;
+        }
+
+        string fileName = args[0];
+
+        if (!File.Exists(fileName))
+        {
+            Console.WriteLine("Tiedostoa ei löytynyt!");
+            return;
+        }
+
+        string fileContent = File.ReadAllText(fileName);
+        Console.WriteLine(fileContent);
+    }
+}
