fae589e0b1492dcef8e76bd949db798fa27d7726
Jere Mäennenä <jere.maennena@gmail.com>
2024-03-06 13:57:45 +0200

    small changes

--- numList/numList/Program.cs
+++ numList/numList/Program.cs
@@ -1,28 +1,31 @@
-﻿using System;
+﻿/* Write a program that asks the user to enter integers until the program receives a negative number as input.
+ * The input numbers should be added to a list of type int (List) called numbers. */
+
+using System;
 using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        List<int> numerot = new List<int>(); // List-esittely
 
         int syote;
-        while (true)
+        List<int> numerot = new List<int>();
+        do
         {
             Console.Write("Syötä numero: ");
             syote = int.Parse(Console.ReadLine());
-            if (syote < 0)
-                break;
-            // Elementtien lisäys listalle
-            numerot.Add(syote);
-        }
+
+            if (syote >= 0)
+            {
+                numerot.Add(syote);
+            }
+        } while (syote >= 0);
 
         Console.Write("Syöttämäsi numerot: ");
         foreach (int numero in numerot)
         {
             Console.Write(numero + " ");
         }
-        Console.WriteLine();
     }
 }
